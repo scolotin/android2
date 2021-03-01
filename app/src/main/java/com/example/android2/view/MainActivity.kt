66c77286cbb32f355fe26b8355c9f32e227f9fb9
Scolotin Alexey <scolotin@gmail.com>
2021-03-02 01:24:20 +0300

    Реализовано боковое меню. Добавлен фрагмент настроек приложения и сохранение их через SharedPreferences

--- app/src/main/java/com/example/android2/view/MainActivity.kt
+++ app/src/main/java/com/example/android2/view/MainActivity.kt
@@ -9,11 +9,19 @@ import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import com.example.android2.R
+import com.example.android2.databinding.ActivityMainBinding
 import com.example.android2.view.main.MainFragment
+import com.example.android2.view.settings.SettingsFragment
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             (context as? Activity)?.showSnackbar(getString(R.string.connection_warning_msg), Snackbar.LENGTH_LONG)
@@ -22,7 +30,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater.apply {
+            inflate(R.layout.activity_main, null)
+        })
+        setContentView(binding.root)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
@@ -30,6 +43,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.nav_drawer_open, R.string.nav_drawer_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navView.setNavigationItemSelectedListener(NavigationView.OnNavigationItemSelectedListener {
+            if (navigateFragment(it.itemId)) {
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+                return@OnNavigationItemSelectedListener true
+            }
+            return@OnNavigationItemSelectedListener false
+        })
+    }
+
+    private fun navigateFragment(id: Int): Boolean {
+        return when (id) {
+            R.id.action_settings -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, SettingsFragment.newInstance())
+                    .addToBackStack("")
+                    .commitAllowingStateLoss()
+                true
+            }
+            else -> false
+        }
     }
 }
 
